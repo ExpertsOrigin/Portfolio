8f75e7e33997f83d683549ab223989318bc5d5a0
Muhammad Noman <nomi.bsit@gmail.com>
2024-06-25 17:12:50 +0500

    changed added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const docs = ReactDOM.createRoot(document.getElementById('docs'));
-docs.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
